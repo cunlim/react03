7cd8464ccea0c0f5416debdf7036ee49b7a983cd
cunlim <zangcunlim@gmail.com>
2024-11-18 05:22:19 +0900

    React 09 - JSX 속성과 스타일링 03

---
+++ src/ButtonA.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonA.css';
+
+function ButtonA() {
+    return (
+        <button className="button">
+            Button A
+        </button>
+    )
+}
+
+export default ButtonA;
---
+++ src/ButtonB.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonB.css';
+
+function ButtonB() {
+    return (
+        <button className="button">
+            Button A
+        </button>
+    )
+}
+
+export default ButtonB;
