e532f4351de09738c863fe5a39618d0ca17ab355
cunlim <zangcunlim@gmail.com>
2024-11-18 03:33:04 +0900

    React 08 - 조건부 렌더링과 리스트 렌더링 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import './App.css'
 
-function getUserContent( userStatus ) {
-  return (
-    userStatus === "admin" ? (
-      <>
-        <h2>Admin Dashboard</h2>
-        <button>Manage Users</button>
-      </>
-    ) : userStatus === "member" ? (
-      <p>Welcome to the community!</p>
-    ) : (
-      <a href="/signup">Sign up here</a>
-    )
-  );
-}
+const hasMessage = true;
+const message = "Hello, World!";
 
 function App() {
   return (
     <>
-      {getUserContent("admin")}
+      {hasMessage && message}
     </>
   )
 }
