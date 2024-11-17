338b16e2f65007d2eab4b264a56d19271d25be70
cunlim <zangcunlim@gmail.com>
2024-11-18 03:36:56 +0900

    React 08 - 조건부 렌더링과 리스트 렌더링 05

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 
 const hasMessage = true;
-const message = "Hello, World!";
+const message = null;
+// const message = "";
+// const message = "Hello!";
 
 function App() {
   return (
     <>
       {hasMessage && <h2>You have new messages!</h2>}
       {message && <p>Message: {message}</p>}
+
+      <p>Message: {message ?? <em>No message</em>}</p>
+      <p>Message: {message || <em>Empty</em>}</p>
     </>
   )
 }
