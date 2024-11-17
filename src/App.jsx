2363a32a8891da9921aac26f5c756ad3aa964f12
cunlim <zangcunlim@gmail.com>
2024-11-18 05:06:59 +0900

    React 09 - JSX 속성과 스타일링 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,67 @@
 import './App.css'
+import reactLogo from './assets/react.svg'
 
 function App() {
 
-  const books = [
-    { id: 1, title: "React Basics", published: true, publisher: "Manning" },
-    { id: 2, title: "Advanced Hooks", published: false, publisher: "OReilly" },
-    { id: 3, title: "JSX in Depth", published: true, publisher: "Packt" },
-  ];
-  const publisheds = books.filter(book => book.published);
+  const disableInput = true;
+  const reactLogoAlt = "React Logo";
+  const divStyle = {
+    backgroundColor: "lightblue",
+    margin: "12px",
+    padding: "20px",
+    borderRadius: "8px"
+  };
 
   return (
     <>
-      {publisheds.length > 0 && <h2>Published Books</h2>}
-      {publisheds.length ? publisheds.map(book => (
-        <article key={book.id}>
-          <strong>{book.title}</strong>
-          <em> - {book.publisher}</em>
-        </article>
-      )) : <p>No published books found.</p>}
+      <label
+        htmlFor="username"        // for
+      >
+        Username:
+      </label>
+      <input
+        type="text"
+        id="username"
+
+        className="input-field"   // class
+        autoComplete="off"        // autocomplete
+
+        maxLength={20}            // maxlength
+        spellcheck={true}         // Warning: Invalid DOM property `spellcheck`. Did you mean `spellCheck`? Error Component Stack
+        readOnly={false}          // readonly
+        tabIndex={0}              // tabindex
+
+        disabled={disableInput}
+        placeholder={disableInput ? "DISABLED" : "Enter your name."}
+      />
+
+      <img
+        src={reactLogo}
+        alt={reactLogoAlt}
+        width={256}
+        height={228}
+      />
+
+      <span style={
+        {
+          fontWeight: "bold",
+          fontStyle: "italic"
+        }
+      }>
+        Bold & Italic
+      </span>
+
+      <div style={divStyle}>
+        DIV 1
+      </div>
+      <div style={{
+        ...divStyle,
+        backgroundColor: 'lightgreen',
+        color: "darkblue",
+        fontWeight: "bold"
+      }}>
+        DIV 2
+      </div>
     </>
   )
 }
