7c98725d65707730fa35fc2d8c3b6e4a249c3b7a
cunlim <zangcunlim@gmail.com>
2024-11-18 05:14:19 +0900

    React 09 - JSX 속성과 스타일링 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,28 @@
 import './App.css'
-import reactLogo from './assets/react.svg'
 
 function App() {
 
-  const disableInput = true;
-  const reactLogoAlt = "React Logo";
-  const divStyle = {
-    backgroundColor: "lightblue",
-    margin: "12px",
-    padding: "20px",
-    borderRadius: "8px"
+  const styleA = {
+    color: "darkred",
+    fontweight: "bold"
   };
+  const styleB = {
+    color: "navy",
+    textDecoration: "underline"
+  };
+  const isPrimary = true;
 
   return (
     <>
-      <label
-        htmlFor="username"        // for
-      >
-        Username:
-      </label>
-      <input
-        type="text"
-        id="username"
-
-        className="input-field"   // class
-        autoComplete="off"        // autocomplete
-
-        maxLength={20}            // maxlength
-        spellcheck={true}         // Warning: Invalid DOM property `spellcheck`. Did you mean `spellCheck`? Error Component Stack
-        readOnly={false}          // readonly
-        tabIndex={0}              // tabindex
-
-        disabled={disableInput}
-        placeholder={disableInput ? "DISABLED" : "Enter your name."}
-      />
-
-      <img
-        src={reactLogo}
-        alt={reactLogoAlt}
-        width={256}
-        height={228}
-      />
-
-      <span style={
-        {
-          fontWeight: "bold",
-          fontStyle: "italic"
-        }
-      }>
-        Bold & Italic
-      </span>
-
-      <div style={divStyle}>
-        DIV 1
+      <div style={isPrimary ? styleA : styleB}>
+        This text has dynamic styling
       </div>
-      <div style={{
-        ...divStyle,
-        backgroundColor: 'lightgreen',
-        color: "darkblue",
-        fontWeight: "bold"
+      <span style={{
+        fontSize: isPrimary ? "1.5em" : "1em",
+        opacity: isPrimary ? 1 : 0.5
       }}>
-        DIV 2
-      </div>
+        So does this text
+      </span>
     </>
   )
 }
