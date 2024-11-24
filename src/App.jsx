dde393c3c8daba55b31353ed746268119366199a
cunlim <zangcunlim@gmail.com>
2024-11-25 05:08:07 +0900

    React 10 - 컴포넌트의 Props 05

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import './App.css'
 
 import InfoCard from './InfoCard'
 
-const cardData1 = {
-  title: "Props in React",
-  content: "Props pass data from one component to another.",
-  author: "Alice"
-};
-const cardData2 = {
-  title: "React Composition",
-  content: "Composition makes your components more reusable"
-};
+const cards = [
+  {
+    idx: 1,
+    title: "Props in React",
+    content: "Props pass data from one component to another.",
+    author: "Alice"
+  }, {
+    idx: 2,
+    title: "React Composition",
+    content: "Composition makes your components more reusable"
+  }
+]
 
 function App() {
 
   return (
     <>
-      <InfoCard {...cardData1} />
-      <InfoCard {...cardData2} />
+      {cards.map(cardData => (
+        <InfoCard key={cardData.idx} {...cardData} />
+      ))}
     </>
   )
 }
